a2eec0af263f3b9131bfc138831df87fca59dc84
Fatih YELBOĞA <fatihyelbogaaa@gmail.com>
2023-04-27 02:23:16 +0300

    .

--- SporganizeApp/SporganizeApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SporganizeApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
